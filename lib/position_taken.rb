9bb3780848669d51bcd22aa53c52127886c07c3a
Gabriel Onike <onikegabriel@gmail.com>
2020-03-31 19:13:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
 # code your #position_taken? method here!
 
-def position_taken?(board, index)
+def position_taken?(board, index ="X"||"O")
   if "#{board[0]}"==" "||"#{board[1]}"==" "||"#{board[2]}"==" "||"#{board[3]}"==" "||"#{board[4]}"==" "||"#{board[5]}"==" "||"#{board[6]}"==" "||
       "#{board[7]}"==" "||"#{board[8]}"==" "||"#{board[9]}"==" "
     false
-  else  !("#{board[0]}"==" "||"#{board[1]}"==" "||"#{board[2]}"==" "||"#{board[3]}"==" "||"#{board[4]}"==" "||"#{board[5]}"==" "||"#{board[6]}"==" "||
-      "#{board[7]}"==" "||"#{board[8]}"==" "||"#{board[9]}"==" " )
+  else  "#{board[0]}"=="X"||"O"||"#{board[1]}"=="X"||"O"||"#{board[2]}"==("X"||"O")||"#{board[3]}"==("X"||"O")||"#{board[4]}"==("X"||"O")||
+  "#{board[5]}"==("X"||"O")||"#{board[6]}"==("X"||"O")||"#{board[7]}"==("X"||"O")||"#{board[8]}"=="X"||"O"||"#{board[9]}"==("X"||"O") 
     true
 end
 end
