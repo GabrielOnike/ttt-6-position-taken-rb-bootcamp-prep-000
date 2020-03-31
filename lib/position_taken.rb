c983cfcaadbc08aa8dfe36086c1afebc3ed4e9fd
Gabriel Onike <onikegabriel@gmail.com>
2020-03-31 19:14:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 
-def position_taken?(board, index ="X"||"O")
+def position_taken?(board, index=("X"||"O") )
   if "#{board[0]}"==" "||"#{board[1]}"==" "||"#{board[2]}"==" "||"#{board[3]}"==" "||"#{board[4]}"==" "||"#{board[5]}"==" "||"#{board[6]}"==" "||
       "#{board[7]}"==" "||"#{board[8]}"==" "||"#{board[9]}"==" "
     false
