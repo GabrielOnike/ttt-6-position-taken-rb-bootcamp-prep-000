2db508f4ef0946ba42309238b4de056356d43a6d
Gabriel Onike <onikegabriel@gmail.com>
2020-03-31 19:05:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,6 @@ def position_taken?(board, index)
   if "#{board[0]}"==" "||"#{board[1]}"==" "||"#{board[2]}"==" "||"#{board[3]}"==" "||"#{board[4]}"==" "||"#{board[5]}"==" "||"#{board[6]}"==" "||
       "#{board[7]}"==" "||"#{board[8]}"==" "||"#{board[9]}"==" "
     false
-  end
 if  "#{board[0]}"=="X"||"O"||"#{board[1]}"=="X"||"O"||"#{board[2]}"==("X"||"O")||"#{board[3]}"==("X"||"O")||"#{board[4]}"==("X"||"O")||
   "#{board[5]}"==("X"||"O")||"#{board[6]}"==("X"||"O")||"#{board[7]}"==("X"||"O")||"#{board[8]}"=="X"||"O"||"#{board[9]}"==("X"||"O")
     true
